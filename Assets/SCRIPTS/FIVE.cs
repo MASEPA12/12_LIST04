f1af0cabca7ca9c3b00d37389a28ba92fbe1fa98
MASEPA12 <114388814+MASEPA12@users.noreply.github.com>
2022-12-13 14:23:14 +0100

    ???

--- Assets/SCRIPTS/FIVE.cs
+++ Assets/SCRIPTS/FIVE.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class FIVE : MonoBehaviour
 {
+    //first we set the bounds; for example 2 and -2 is the bouds of x
+    public float X; //BOUND front/back
+    public float Z; //bound right/left
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isOutOfBounds(X, Z))
+        {
+            Debug.Log($"The player is out of bounds");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private bool isOutOfBounds(float x, float z)
+    {
+        if(transform.position.x > x || transform.position.x < -x)
+        {
+            return true;
+        }
+        if(transform.position.z > z || transform.position.z < -z)
+        {
+            return true;
+        }
+        return false;
     }
 }
--- Assets/SCRIPTS/FOUR.cs
+++ Assets/SCRIPTS/FOUR.cs
@@ -3,16 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FOUR : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+{   //we set the array with an index that we'll use it to get a random number between it's start and it's end.
+    public string[] Array;
+    private int ArrayIndex;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        //randomElement();
     }
+    //private int randomElement()
+    //{                          
+        /*ArrayIndex = Random.Range(0, Array.Length);
+        string nameOfArray;
+        //nameofarray es igual al Nº aleatri de s'array's name!!!!
+        nameOfArray = Array[ArrayIndex].name;
+        Debug.Log($"The aleatorian element is {ArrayIndex}");
+        return ArrayIndex ;*/
+    //}
 }
